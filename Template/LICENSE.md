2a9a922961b1910f7c5b40a6fffe89eda56527cb
Lodomo <lodomo.dev@gmail.com>
2025-05-13 11:30:32 -0700

    Fixing DeltaTime

--- Template/LICENSE.md
+++ Template/LICENSE.md
@@ -7,6 +7,6 @@ I'm not messing around with verbose legalese, so here's the deal.
 
 2. You can make a game from it and sell it.
 
-3. You should not sell this template as a template.
+3. You should not *sell* this template as a template.
 
 4. If you use it, let me know! I'd be stoked to hear it.
--- Template/source/scripts/classes/delta_time.lua
+++ Template/source/scripts/classes/delta_time.lua
@@ -1,31 +1,33 @@
---[[ Delta Time Documentation
---]]
-DeltaTime = {}
-DeltaTime.__index = DeltaTime
-setmetatable(DeltaTime, { __index = Class })
+DeltaTime = Class("deltatime")
 
--- Set Data Members
-function DeltaTime:new()
-    local obj = Class:new()
-    setmetatable(obj, self)
-    obj.current = 0
-    obj.last = 0
-    obj.delta = 0
-    obj.getTime = GetCurrentTimeMilliseconds
-    return obj
+function DeltaTime:init()
+    self.current = 0
+    self.last = 0
+    self.delta = 0
+    self.correct_overflow = 2^32
+    self.now = GetCurrentTimeMilliseconds
 end
 
--- Set Methods
+--[[ TODO OVERFLOW DETECTION
 function DeltaTime:update()
-    self.current = self.getTime()
-    self.delta = (self.current - self.last) / 1000
+    self.current = self.now()
+
+    self.delta = self.current - self.last
+    if self.delta < 0 then
+        -- Handle overflow
+        self.delta = self.delta 
+    end
+
+
     self.last = self.current
+    return self.delta
 end
+--]]
 
 function DeltaTime:get()
     return self.delta
 end
 
 function DeltaTime:peek()
-    return (self.getTime() - self.last) / 1000
+    return (self.now() - self.last) / 1000
 end
